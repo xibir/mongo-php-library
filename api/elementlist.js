7b352f079bed804a086a9c577e1cd416d3d98a20
Jeremy Mikola <jmikola@gmail.com>
2015-03-24 00:10:25 -0500

    Update documentation

--- api/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Exception"],["c","InvalidArgumentException"],["c","Iterator"],["c","LogicException"],["c","MongoDB\\Client"],["c","MongoDB\\Collection"],["c","MongoDB\\Database"],["c","MongoDB\\DeleteResult"],["c","MongoDB\\InsertManyResult"],["c","MongoDB\\InsertOneResult"],["c","MongoDB\\UpdateResult"],["c","Traversable"]];
