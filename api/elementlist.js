9c69dbd6bde1ea1890bf857654d4825140a566b7
Jeremy Mikola <jmikola@gmail.com>
2015-03-24 01:08:27 -0400

    Restore API documentation

---
+++ api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Exception"],["c","InvalidArgumentException"],["c","Iterator"],["c","LogicException"],["c","MongoDB\\Client"],["c","MongoDB\\Collection"],["c","MongoDB\\Database"],["c","MongoDB\\DeleteResult"],["c","MongoDB\\InsertManyResult"],["c","MongoDB\\InsertOneResult"],["c","MongoDB\\UpdateResult"],["c","Traversable"]];
